793098c952d8e4acb8dd6f4b87f51f7651cf53fd
chunwarayut <warayut2555@gmail.com>
2021-06-11 19:37:59 +0700

    Delete bootstrap/scss/bootstrap.scss

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-import "bootstrap/scss/bootstrap.scss";
-import "assets/scss/paper-kit.scss?v=1.3.0";
-import "assets/demo/demo.css?v=1.3.0";
+import "./assets/scss/paper-kit.scss?v=1.3.0";
+import "./assets/demo/demo.css?v=1.3.0";
 
 import Index from "views/Index";
 import NucleoIcons from "views/NucleoIcons";
