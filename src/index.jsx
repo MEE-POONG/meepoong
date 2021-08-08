ba6baabb1ab9234928864b1a7a44572e3bf8f59a
warayut.ta <warayut.ta@princhealth.com>
2021-08-08 21:50:11 +0700

    update

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-// import "bootstrap/scss/bootstrap.scss";
+import "bootstrap/scss/bootstrap.scss";
 import "assets/scss/paper-kit.scss?v=1.3.0";
 import "assets/demo/demo.css?v=1.3.0";
 
