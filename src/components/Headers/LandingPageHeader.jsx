66a9157750b70375de6edf02e030ba1aa3baf850
warayut.ta <warayut.ta@princhealth.com>
2021-08-08 21:26:58 +0700

    new update

--- src/components/Headers/LandingPageHeader.jsx
+++ src/components/Headers/LandingPageHeader.jsx
@@ -44,7 +44,7 @@ function LandingPageHeader() {
     return (
       <div className="motto text-center">
         {/* <img src={require("assets/img/logom.png").default} width="145" alt="MEE POONG GROUP LOGO" className="rounded-circle bg-white" /> */}
-        <img src={require("assets/img/logomm.png").default} width="245" alt="MEE POONG GROUP LOGO" className="shadow-logo" />
+        <img src={require("assets/img/logomm.png").default} width="285" alt="MEE POONG GROUP LOGO" className="shadow-logo" />
         <p className="p-2"> </p>
         {desktopContent()}
         {mobileContent()}
@@ -59,7 +59,7 @@ function LandingPageHeader() {
 
     function desktopContent() {
       return <BrowserView>
-        <h2 className="font-weight-bold">Coder <span className="text-coder"> | </span>  Designer <span className="text-designer"> | </span>  Thinker <span className="text-thinker"> | </span>  Blogger <span className="text-blocker"> | </span>  Marketer</h2>
+        <h3 className="font-weight-bold">Coder <span className="text-coder"> | </span>  Designer <span className="text-designer"> | </span>  Thinker <span className="text-thinker"> | </span>  Blogger <span className="text-blocker"> | </span>  Marketer</h3>
       </BrowserView>;
     }
   }
