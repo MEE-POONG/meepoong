9916aea4e673b712221758ad20e6d7c26ed14554
warayut.ta <warayut.ta@princhealth.com>
2021-09-19 22:47:18 +0700

    blog detail

--- src/elements/BlogDetails.jsx
+++ src/elements/BlogDetails.jsx
@@ -1,24 +1,85 @@
 import React from "react";
 import PageHelmet from "../component/common/Helmet";
+import { Helmet } from 'react-helmet'
+
 // import { FiClock, FiUser, FiMessageCircle, FiHeart } from "react-icons/fi";
 // import { Link } from "react-router-dom";
 import ScrollToTop from "react-scroll-up";
 import { FiChevronUp } from "react-icons/fi";
 import Header from "../component/header/Header";
 import Footer from "../component/footer/Footer";
-import BlogContent from "./blog/BlogContent";
+
 import { useParams } from "react-router";
 
+import {
+  database,
+} from '../firebase.js';
+
+
+import Editor from '@react-page/editor';
+
+// import the main css, uncomment this: (this is commented in the example because of https://github.com/vercel/next.js/issues/19717)
+import '@react-page/editor/lib/index.css';
+
+// The rich text area plugin
+import slate from '@react-page/plugins-slate';
+// image
+import image from '@react-page/plugins-image';
+
+// Stylesheets for the rich text area plugin
+// uncomment this
+import '@react-page/plugins-slate/lib/index.css';
+
+// Stylesheets for the imagea plugin
+import '@react-page/plugins-image/lib/index.css';
+
+// Define which plugins we want to use.
+const cellPlugins = [slate(), image];
+
+
 const BlogDetails = () => {
-  const PostList = BlogContent.slice(0, 6);
-  const Title = "Title News";
+
+
+
+  // const Title = "Title News";
   const { id } = useParams();
-  const postdetail = PostList.find((e) => (+e.id === id));
-  console.log(id);
-  console.log(postdetail);
+  const blogsRef = database.ref('/blogs/' + id);
+
+
+
+
+
+  const [newsDetail, setNewsDetail] = React.useState([])
+  React.useEffect(() => {
+
+    blogsRef.on('value', (snapshot) => {
+      const data = snapshot.val();
+
+      setNewsDetail(data)
+    });
+    // eslint-disable-next-line
+  }, [id])
+
+
+  const { description, detail, image, title } = newsDetail
+
   return (
     <React.Fragment>
-      <PageHelmet pageTitle="Blog Details/" />
+      <PageHelmet pageTitle={`MEE POONG BLOG DETAIL - ${title}`} />
+      <Helmet>
+        <title>{`MEE POONG BLOG DETAIL - ${title}`}</title>
+
+        <meta property="og:title" content={`MEE POONG BLOG DETAIL - ${title}`} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+
+        <meta name="twitter:title" content={`MEE POONG BLOG DETAIL - ${title}`} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="og:site_name" content={`MEE POONG BLOG DETAIL - ${title}`} />
+        <meta name="twitter:image:alt" content={`MEE POONG BLOG DETAIL - ${title}`} />
+      </Helmet>
       <Header
         headertransparent="header--transparent"
         s
@@ -35,7 +96,7 @@ const BlogDetails = () => {
           <div className="row">
             <div className="col-lg-12">
               <div className="blog-single-page-title text-center pt--100">
-                <h2 className="title theme-gradient">{Title}</h2>
+                <h2 className="title theme-gradient">{title}</h2>
               </div>
             </div>
           </div>
@@ -50,20 +111,27 @@ const BlogDetails = () => {
             <div className="col-lg-12">
               <div className="inner-wrapper">
                 <div className="inner">
-                  <div className="thumbnaili text-center" >
+                  <div className=" text-center" >
                     <img
-                      src={`/assets/images/blog/blog-${postdetail.images}.jpg`}
-                      alt="Blog Images"
-                      
+                      src={image}
+                      alt={title}
                     />
                   </div>
                   <p className="mt--40"></p>
-                  <p>{postdetail.category}</p>
+                  {/* <p>{postdetail.category}</p> */}
                   <blockquote className="rn-blog-quote">
-                    {postdetail.title}
+                    {title}
                   </blockquote>
 
-                  <p className="mt--25 mt_sm--5">{postdetail.category}</p>
+                  <p className="mt--25 mt_sm--5">
+                    <Editor
+                      id="detail"
+                      name="detail"
+                      placeholder="เนื้อหา"
+                      cellPlugins={cellPlugins}
+                      value={detail}
+                      readOnly
+                    /></p>
                 </div>
               </div>
             </div>
